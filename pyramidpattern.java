1689739e392a2dfca98a17507e5f28a93310e5ab
s-vinaya <131242389+s-vinaya@users.noreply.github.com>
2023-04-20 10:27:29 +0530

    pyramidpattern.java

---
+++ pyramidpattern.java
@@ -0,0 +1,22 @@
+public class edureka
+{
+public static void pyramid pattern(int n)
+{
+for (int i=0;,i<n;i++)
+{
+for (int j=n-1;j>1;j--)
+{
+System.out.println(" ");
+}
+for(int j=0;j<1;j++)
+coloumns
+{
+System.out.println(" *");
+}
+}
+public static void main (String args[])
+{
+int n=5;
+pyramid pattern(n);
+}
+}
